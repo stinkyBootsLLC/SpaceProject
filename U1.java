ab9ad783bc6cdf2d0ceec7db66d2c5e29d38e837
stinkyBootsLLC <stinkyBootsLLC@gmail.com>
2019-03-10 13:18:28 -0400

    define the U1 rocket

--- U1.java
+++ U1.java
@@ -12,12 +12,6 @@
     in the rocket.
 
 
-
-
-
-
-
-
 */
 package spaceproject;
 
@@ -26,10 +20,17 @@ package spaceproject;
  * @author eduardo
  */
 public class U1 extends Rocket{
-    
-    public static int u1TotalWeight;
+    // declare variables
+
+    public static int U1_totalWeight;// cargo + rocket
 
     public U1() {
+        super.rocketCost = 100000000;  // It costs $100 Million to build
+        super.rocketWeight = 9071.85f; // weighs 10 tonnes
+        super.cargoMaxWeight = 16329.33f;  // = 18 tonnes of cargo = 16,329.33 kg
+        super.totalWeightLimit = rocketWeight + cargoMaxWeight;
+        super.chanceOfLandingCrash = 0;// It has a slim chance of crashing while landing 
+        super.chanceOfLaunchExplosion = 0; // but a bigger chance of exploding when launching
     }// default constructor
     
     
@@ -68,14 +69,14 @@ public class U1 extends Rocket{
 
     public void calculateTotalWeight(){
         
-        Item item = new Item();
+       // Item item = new Item();
         
         // 907.185 kg =  1 Ton
         // MAX weight = 18 tonnes of cargo = 16,329.33 kg
         
-        int itemWeight = item.getWeight();
+        //int itemWeight = item.getWeight();
         
-        u1TotalWeight = itemWeight;
+       // u1TotalWeight = itemWeight;
         
         
         // bring in the Item
@@ -87,6 +88,13 @@ public class U1 extends Rocket{
         
         
     }// end calculateTotalWeight
+
+    @Override
+    public String toString() {
+        return "U1 rocket \n"+super.toString() + "totalWeightLimit = " + totalWeightLimit;
+    }
+    
+    
     
     
     
